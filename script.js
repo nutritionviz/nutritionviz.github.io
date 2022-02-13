c90c29373ec2a5cdee71e6555fc0124425bf7289
nutritionviz <99536034+nutritionviz@users.noreply.github.com>
2022-02-13 16:29:37 +0000

    nice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("hello")
-$.get("./data.csv", function (CSVdata) {
+$.get("https://nutritionviz.github.io/data.csv", function (CSVdata) {
     // CSVdata is populated with the file contents
     // ready to be converted into an Array
     data = $.csv.toArray(CSVdata);
